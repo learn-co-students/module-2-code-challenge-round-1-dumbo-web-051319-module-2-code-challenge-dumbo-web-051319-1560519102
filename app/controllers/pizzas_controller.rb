70ea858ce09d2b598156b64a95f54e769e812633
github username <github email address>
2019-06-14 11:11:28 -0400

    i messed up :(

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -2,4 +2,23 @@ class PizzasController < ApplicationController
   def index
     @pizzas = Pizza.all
   end
+
+  def show
+    @pizza = Pizza.find(params[:id])
+  end
+
+  def new
+    @pizza = Pizza.new
+  end
+
+  def create
+    pizza = Pizza.create(self_params)
+    redirect_to pizza
+  end
+
+  private
+
+  def self_params
+    params.require(:pizza).permit(:name, :ingredients, :restaurant_id)
+  end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,4 @@
 class Restaurant < ApplicationRecord
-  
+  has_many :pizzas
+  has_many :members, through: :pizzas
 end
