7d62d2f176a7227c316e4c62f7474289e2581d69
Kenton Archer <kenton8654@yahoo.com>
2019-06-14 11:11:37 -0400

    Better luck next time K

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,37 @@
 class RestaurantsController < ApplicationController
+   before_action :find_restaurant,  only:[:show,:edit,:update,:delete]
+
+
+
   def index
     @restaurants = Restaurant.all
   end
+
+  def show
+    # @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant.Restaurant.create(restaurant_params)
+
+  end
+
+
+
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
+  end
+
+
 end
