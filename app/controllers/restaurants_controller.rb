8bd6a02b77b9fa33b677640a09ed0f3422d76814
Stefanos <stefanosu422@gmail.com>
2019-06-14 11:11:39 -0400

    Another one

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,32 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+
+def show 
+  @restaurant = Restaurant.find(params[:id]) 
+end
+
+def create 
+  restaurant = Restaurant.create(restaurant_params)
+  
+end
+
+def new 
+  restaurant = Restaurant.new 
+end
+
+
+def edit 
+  
+  
+end
+
+private 
+
+def restaurant_params 
+  params.require(:resturant).permit(:name, :address)
+end 
+
+
 end
--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,15 @@
 class Pizza < ApplicationRecord
-end
+    belongs_to :restaurant
+
+
+validates :pizza, presence: true 
+validates :pizza, uniqueness: true
+end 
+
+
+
+
+
+
+# 6. I should not be able to create a pizza with the 
+# same name as a pizza that already exists.
