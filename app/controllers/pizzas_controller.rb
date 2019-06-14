ffce3000f3f9631c472715741c3e4efe0c07a72f
Toni Lamont <tlam212@gmail.com>
2019-06-14 11:14:23 -0400

    mod2 code challenge

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -2,4 +2,28 @@ class PizzasController < ApplicationController
   def index
     @pizzas = Pizza.all
   end
+
+  def show
+    @pizzas = Pizza.find(params[:id])
+  end
+
+  def new
+    @pizza = Pizza.new
+    @restaurants = Restaurant.all
+  end
+
+  def create
+    @pizza = Pizza.create(pizza_params)
+    redirect_to @pizzas
+
+
+# just realized pizza seed isnt there, try to add, not working. not enough time to figureout error
+  end
+
+
+
+  private
+  def pizza_params
+    params.require(:pizza).permit(:name, :ingredients)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 #
 #   1
 Restaurant.destroy_all
- 
+
 Restaurant.create!([{
   name: "Sottocasa NYC",
   address: "298 Atlantic Ave, Brooklyn, NY 11201",
@@ -21,3 +21,5 @@ Restaurant.create!([{
   name: "San Matteo NYC",
   address: "1559 2nd Ave, New York, NY 10028"
 }])
+
+Pizza.create(name: "Pepperoni", ingredients:"cheese and Pepperoni")
