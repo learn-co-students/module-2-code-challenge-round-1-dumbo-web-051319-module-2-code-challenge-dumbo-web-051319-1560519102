70d0550cd553a3f4ae745ad6dcd046524b72d597
othaderek <otha.hernandez@gmail.com>
2019-06-14 11:11:41 -0400

    Mostly done.

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,7 @@
 class Pizza < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :name, presence: true, uniqueness: true
+  validates :ingredients, presence: true
+  validates :restaurant_id, presence: true
 end
