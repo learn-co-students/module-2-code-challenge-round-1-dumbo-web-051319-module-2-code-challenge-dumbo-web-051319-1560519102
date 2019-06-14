1a43890eab64da9e50087f7da8a9c5fa12b0c37d
Rachel R <rrrachelrath@gmail.com>
2019-06-14 11:11:29 -0400

    rachel-final

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,25 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
+    if params[:search]
+      @restaurants = Restaurant.find_by(name: params[:search])
+
+      # @restaurants = Restaurant.find_by(name: :q)
+      # if !@restaurants
+      #   return Restaurant.all
+      # end
+    end
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
+  end
+
 end
