d07900674215ff2dcbdd44721890d044c5f67c9f
Joseph <josephgavinsyverson@gmail.com>
2019-06-14 10:22:38 -0400

    all deliverables except 7, I think

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -2,4 +2,20 @@ class PizzasController < ApplicationController
   def index
     @pizzas = Pizza.all
   end
+  def show
+    @pizza = Pizza.find(params[:id])
+  end
+  def new
+    @pizza = Pizza.new
+  end
+  def create
+    @pizza = Pizza.create(strong_params)
+    redirect_to pizza_path(@pizza)
+  end
+
+  private
+
+  def strong_params
+    params.require(:pizza).permit(:name, :ingredients, :restaurant_id)
+  end
 end
