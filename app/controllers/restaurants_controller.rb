5ba5d91b23934d31458ea1468268a33ec4b68617
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-06-14 11:11:13 -0400

    functioning crud pizza app

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,15 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
+    if params[:search]
+      # @restaurants = Restaurant.where(name: params[:search])
+      @restaurants = Restaurant.where('name lIKE ?', "%#{params[:search]}%")
+    end
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @pizzas = @restaurant.pizzas
+  end
+
 end
